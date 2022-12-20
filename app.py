c8659329487388f22121f67b980150a18f61bf12
ale10bb <ale10@qq.com>
2022-12-20 19:34:37 +0800

    使用原生API重构

--- app.py
+++ app.py
@@ -1,132 +1,180 @@
 # -*- coding: UTF-8 -*-
-# 读取配置
-import os.path
-from configparser import ConfigParser
-config = ConfigParser()
-config.read(os.path.join('conf','wxwork.conf'), encoding='UTF-8')
-conf = dict(config._sections)
-del config
-
 # Flask程序及函数
-from flask import Flask, request
+import ipaddress
+from flask import Flask, request, g, abort
 import xml.etree.ElementTree as ET
 import requests
 from weworkapi_callback.WXBizMsgCrypt3 import WXBizMsgCrypt
 
+shm = {}
 
-app = Flask(__name__)
-app.logger.setLevel('DEBUG')
+def init():
+    global shm
+    # 读取配置
+    import os.path
+    from configparser import ConfigParser
+    config = ConfigParser()
+    config.read(os.path.join('conf','wxwork.conf'), encoding='UTF-8')
+    shm = dict(config._sections)
+
+    app = Flask(__name__)
+    app.logger.setLevel('INFO')
+    return app
+
+app = init()
 
 
-def get_client_ip(request):
-    x_forwarded_for = request.headers.get('X-Forwarded-For')
-    return x_forwarded_for.split(',')[0] if x_forwarded_for else request.remote_addr
+@app.before_request
+def before_request():
+    g.client_ip = request.headers['X-Forwarded-For'].split(',')[0] if 'X-Forwarded-For' in request.headers else request.remote_addr
+    ipaddress.ip_address(g.client_ip)
 
 
-@app.route('/push/<module>', methods=['GET', 'POST'])
-def push_rm(module):
-    app.logger.info('request from "{}"/<{}>'.format(get_client_ip(request), module))
-
-    # URL中包含msg_signature、timestamp、nonce参数
-    msg_signature = request.args['msg_signature']
-    timestamp = request.args['timestamp']
-    nonce = request.args['nonce']
-    
+@app.route('/push/<module>')
+def verify_URL(module):
     # 支持Http Get请求验证URL有效性
     # https://developer.work.weixin.qq.com/document/path/90930#31-%E6%94%AF%E6%8C%81http-get%E8%AF%B7%E6%B1%82%E9%AA%8C%E8%AF%81url%E6%9C%89%E6%95%88%E6%80%A7
-    if request.method == 'GET':
-        app.logger.info('mode: VerifyURL')
-        echostr = request.args['echostr']
-        wxcpt = WXBizMsgCrypt(conf[module]['token'], conf[module]['encodingaeskey'], conf[module]['corpid'])
-        ret, plain_echostr = wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
-        if ret:
-            app.logger.error("ERR: VerifyURL ret: {}".format(ret))
-            return ret, 500
-        app.logger.info('plain_echostr: {}'.format(plain_echostr))
-        return plain_echostr
+    app.logger.info('mode: VerifyURL/<{}>'.format(module))
+    wxcpt = WXBizMsgCrypt(shm[module]['token'], shm[module]['encodingaeskey'], shm[module]['corpid'])
+    ret, plain_echostr = wxcpt.VerifyURL(
+        request.args['msg_signature'], 
+        request.args['timestamp'], 
+        request.args['nonce'], 
+        request.args['echostr'],
+    )
+    if ret:
+        app.logger.error("ERR in VerifyURL: {}".format(ret))
+        abort(500)
+    app.logger.debug('plain_echostr: {}'.format(plain_echostr))
+    return plain_echostr
 
+
+@app.route('/push/rm', methods=['POST'])
+def handle_rm():
+    app.logger.info('message/<rm> from "{}"'.format(g.client_ip))
     # 支持Http Post请求接收业务数据
     # https://developer.work.weixin.qq.com/document/path/90930#32-%E6%94%AF%E6%8C%81http-post%E8%AF%B7%E6%B1%82%E6%8E%A5%E6%94%B6%E4%B8%9A%E5%8A%A1%E6%95%B0%E6%8D%AE
-    if request.method == 'POST':
-        app.logger.info('mode: DecryptMsg')
-        wxcpt = WXBizMsgCrypt(conf[module]['token'], conf[module]['encodingaeskey'], conf[module]['corpid'])
-        ret, plain_msg = wxcpt.DecryptMsg(request.data, msg_signature, timestamp, nonce)
-        if ret:
-            app.logger.error('ERR: DecryptMsg ret: {}'.format(ret))
-            return ret, 500
-        app.logger.debug('plain_msg: {}'.format(plain_msg))
+    wxcpt = WXBizMsgCrypt(shm['rm']['token'], shm['rm']['encodingaeskey'], shm['rm']['corpid'])
+    ret, plain_msg = wxcpt.DecryptMsg(
+        request.data, 
+        request.args['msg_signature'], 
+        request.args['timestamp'], 
+        request.args['nonce'], 
+    )
+    if ret:
+        app.logger.error('ERR in DecryptMsg: {}'.format(ret))
+        abort(500)
+    app.logger.debug('plain_msg: {}'.format(plain_msg))
 
-        reply_content = ''
-        xml_tree = ET.fromstring(plain_msg)
-        msg_type = xml_tree.find("MsgType").text
-        from_user = xml_tree.find("FromUserName").text
+    reply_content = ''
+    xml_tree = ET.fromstring(plain_msg)
+    msg_type = xml_tree.find("MsgType").text
+    user = xml_tree.find("FromUserName").text
 
-        if module == 'rm':
-            if msg_type == 'event':
-                event = xml_tree.find("Event").text
-                app.logger.debug('[event/{}] message from "{}"'.format(event, from_user))
-                # 处理“菜单事件”
-                if event == 'click':
+    match msg_type:
+        case 'event':
+            event = xml_tree.find("Event").text
+            match event:
+                case 'click':
+                    # 处理“菜单事件”
                     event_key = xml_tree.find("EventKey").text
-                    app.logger.debug('EventKey: {}'.format(event_key))
-                    if event_key == 'RM_KNOCK':
-                        requests.post('{}/api/mail'.format(conf[module]['forward']), json={})
-                        reply_content = '[打机器人]已加入处理队列'
-                    elif event_key == 'RM_QUEUE':
-                        r = requests.post('{}/api2/queue/list'.format(conf[module]['forward']), json={}).json()
-                        assert not r['result'], r['err']
-                        for idx, item in enumerate(r['data']['normal']):
-                            if from_user != item['id']:
-                                continue
-                            if item['status'] == -1:
-                                status = '(跳过1篇)'
-                            elif item['status'] == 0:
-                                status = '空闲'
-                            elif item['status'] == 1:
-                                status = '不审加急'
-                            elif item['status'] == 2:
-                                status = '不审报告'
-                            else:
-                                status = '未知'
-                            reply_content = '===== 状态通知 =====\n\n你的状态: {}\n你的分配顺位: {}{}{}'.format(
-                                status, 
-                                idx + 1 if item['status'] != -1 else 'x', 
-                                ' (+{}页)'.format(item['pages_diff']) if item['pages_diff'] else '',
-                                '\n你当前有{}个审核任务'.format(item['current']) if item['current'] else ''
-                            )
-                    else:
-                        pass
-                elif event == 'subscribe':
-                    r = requests.post('{}/api2/user/search'.format(conf[module]['forward']), json={'id': from_user}).json()
+                    match event_key:
+                        case 'RM_KNOCK':
+                            app.logger.info('{}: event/click/RM_KNOCK'.format(user))
+                            requests.post('{}/api/mail'.format(shm['rm']['forward']), json={})
+                            reply_content = '[打机器人]已加入处理队列'
+                        case 'RM_QUEUE':
+                            app.logger.info('{}: event/click/RM_QUEUE'.format(user))
+                            r = requests.post('{}/api2/queue/list'.format(shm['rm']['forward']), json={}).json()
+                            assert not r['result'], r['err']
+                            for idx, item in enumerate(r['data']['normal']):
+                                if user != item['id']:
+                                    continue
+                                if item['status'] == -1:
+                                    status = '(跳过1篇)'
+                                elif item['status'] == 0:
+                                    status = '空闲'
+                                elif item['status'] == 1:
+                                    status = '不审加急'
+                                elif item['status'] == 2:
+                                    status = '不审报告'
+                                else:
+                                    status = '未知'
+                                reply_content = '===== 状态通知 =====\n\n你的状态: {}\n你的分配顺位: {}{}{}'.format(
+                                    status, 
+                                    idx + 1 if item['status'] != -1 else 'x', 
+                                    ' (+{}页)'.format(item['pages_diff']) if item['pages_diff'] else '',
+                                    '\n你当前有{}个审核任务'.format(item['current']) if item['current'] else ''
+                                )
+                        case _:
+                            app.logger.debug('{}: (unhandled) event/click/{}'.format(user, event_key))
+                case 'subscribe':
+                    app.logger.info('{}: event/subscribe'.format(user))
+                    r = requests.post('{}/api2/user/search'.format(shm['rm']['forward']), json={'id': user}).json()
                     assert not r['result'], r['err']
                     if r['data']['user']:
                         ret = r['data']['user'][0]
                         is_reviewer = '是' if ret['role'] else '否'
                         reply_content = '通知：启用\n用户：{}\n审核人：{}'.format(ret['name'], is_reviewer)
                     else:
-                        reply_content = '通知：启用\n用户：{}（无用户信息）'.format(from_user)
-                # 跳过打开网页的事件
-                elif event == 'view':
-                    app.logger.debug('EventKey: {}'.format(xml_tree.find("EventKey").text))
-                else:
-                    pass
-            else:
-                app.logger.debug('unhandled MsgType [{}] from {}'.format(msg_type, from_user))
+                        reply_content = '通知：启用\n用户：{}（无用户信息）'.format(user)
+                case 'view':
+                    # 跳过打开网页的事件
+                    app.logger.info('{}: event/view -> {}'.format(user, xml_tree.find("EventKey").text))
+                case _:
+                    app.logger.debug('{}: (unhandled) event/{}'.format(user, event))
+        case _:
+            app.logger.debug('{}: (unhandled) {}'.format(user, msg_type))
 
-        # 加密回复信息
-        app.logger.debug('reply_content: {}'.format(reply_content))
-        ret, encrypted_msg = wxcpt.EncryptMsg(
-            '<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{}]]></Content></xml>'.format(
-                from_user,
-                conf[module]['corpid'],
-                timestamp,
-                reply_content
-            ),
-            nonce, 
-            timestamp
-        )
-        if ret:
-            app.logger.error('ERR: EncryptMsg ret: {}'.format(ret))
-            return ret, 500
+    # 加密回复信息
+    app.logger.debug('reply_content: {}'.format(reply_content))
+    ret, encrypted_msg = wxcpt.EncryptMsg(
+        '<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{}]]></Content></xml>'.format(
+            user,
+            shm['rm']['corpid'],
+            request.args['timestamp'],
+            reply_content
+        ),
+        request.args['nonce'], 
+        request.args['timestamp'],
+    )
+    if ret:
+        app.logger.error('ERR in EncryptMsg: {}'.format(ret))
+        abort(500)
 
-        return encrypted_msg
+    return encrypted_msg
+
+
+@app.route('/push/<module>', methods=['POST'])
+def handle_others(module):
+    app.logger.info('message/<{}> from "{}"'.format(module, g.client_ip))
+    # 支持Http Post请求接收业务数据
+    # https://developer.work.weixin.qq.com/document/path/90930#32-%E6%94%AF%E6%8C%81http-post%E8%AF%B7%E6%B1%82%E6%8E%A5%E6%94%B6%E4%B8%9A%E5%8A%A1%E6%95%B0%E6%8D%AE
+    wxcpt = WXBizMsgCrypt(shm[module]['token'], shm[module]['encodingaeskey'], shm[module]['corpid'])
+    ret, plain_msg = wxcpt.DecryptMsg(
+        request.data, 
+        request.args['msg_signature'], 
+        request.args['timestamp'], 
+        request.args['nonce'], 
+    )
+    if ret:
+        app.logger.error('ERR in DecryptMsg: {}'.format(ret))
+        abort(500)
+    app.logger.debug('plain_msg: {}'.format(plain_msg))
+
+    # 加密回复信息
+    ret, encrypted_msg = wxcpt.EncryptMsg(
+        '<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{}]]></Content></xml>'.format(
+            ET.fromstring(plain_msg).find("FromUserName").text,
+            shm[module]['corpid'],
+            request.args['timestamp'],
+            'plain echo',
+        ),
+        request.args['nonce'], 
+        request.args['timestamp'],
+    )
+    if ret:
+        app.logger.error('ERR in EncryptMsg: {}'.format(ret))
+        abort(500)
+
+    return encrypted_msg
